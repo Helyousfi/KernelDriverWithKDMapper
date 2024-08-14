b5ffb818533b8055d706902193fa82cbfefd6aa7
Helyousfi <visioncomputer200@gmail.com>
2024-08-14 22:21:26 +0200

    Added Entry point

--- CPPDriver/CPPDriver/driver.cpp
+++ CPPDriver/CPPDriver/driver.cpp
@@ -1,9 +1,21 @@
 #include <ntddk.h>
 #include <ntifs.h>
-#include <ntddk.h>
 #include <windef.h>
 #include <ntstrsafe.h>
 
-extern "C" NTSTATUS EntryPoint() {
+extern "C" NTSTATUS
+DriverEntry(
+    _In_ PDRIVER_OBJECT   DriverObject,
+    _In_ PUNICODE_STRING  RegistryPath
+)
+{
+    NTSTATUS status;
+    UNICODE_STRING deviceName;
+    UNICODE_STRING symbolicLinkName;
+    PDEVICE_OBJECT deviceObject = NULL;
 
+    UNREFERENCED_PARAMETER(RegistryPath);
+
+
+    return STATUS_SUCCESS;
 }
