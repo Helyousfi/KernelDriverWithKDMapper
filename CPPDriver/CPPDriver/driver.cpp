f5b0235312e300070757fb2c789156711387fb55
Helyousfi <visioncomputer200@gmail.com>
2024-08-17 00:33:59 +0200

    Updated

--- CPPDriver/CPPDriver/driver.cpp
+++ CPPDriver/CPPDriver/driver.cpp
@@ -1,3 +1,10 @@
+/*
+
+The main entry point for the driver. The DriverEntry function is called when the driver is loaded. 
+It makes a call to nullhook::call_kernel_function, which seems to be a hook handler.
+
+*/
+
 #include "hook.h"
 
 extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT driver_object, PUNICODE_STRING reg_path)
